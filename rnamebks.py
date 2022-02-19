613437cd7bcbd967d69e2bb910bd79a0cc3b48ce
wwwmacdoor5 <notfoundtype@gmail.com>
2022-02-19 17:14:53 +0300

    Исправил баги с кодировкой. Теперь может читать любые fb2 файлы.

--- rnamebks.py
+++ rnamebks.py
@@ -1,6 +1,8 @@
-import os, sys, re, argparse, progressbar
+import os, sys, re, argparse
 from colorama import init, Fore, Back, Style
 import epub_meta
+from chardet.universaldetector import UniversalDetector
+from progress.bar import IncrementalBar 
 
 # from PyPDF2 import PdfFileReader
 
@@ -34,49 +36,56 @@ args = parser.parse_args(sys.argv[1:])
 print('PATH:  '+args.path)
 print('----------------------------------------------------------------------')
 
-
-bar = progressbar.ProgressBar(len(list(os.walk(args.path))))
-bar.start()
-time = 0
+bar = IncrementalBar('Файлы ' ,max = len(list(os.walk(args.path))))
 
 
 for dirpath, dirnames, filenames in os.walk(args.path):
 	dirpath+='\\'
-	time+=1
-	bar.update(time)
+	bar.next()
 	for i in filenames:
 		
 		if re.findall('.fb2$',i):
 			try:
-				cont = file.read()
-				bookTitle =  re.findall('<book-title>[а-яА-Я0-9a-zA-Z.,?!:;-_|/*<>\\\' ]+</book-title>', cont)[0].replace('<book-title>', '' ).replace('</book-title>', '')
-				firstName = re.findall('<first-name>[а-яА-Я0-9a-zA-Z.,?!:;-_|/*<>\\\' ]+</first-name>', cont)[0].replace('<first-name>', '' ).replace('</first-name>', '')
-				lastName = re.findall('<last-name>[а-яА-Я0-9a-zA-Z.,?!:;-_|/*<>\\\' ]+</last-name>', cont)[0].replace('<last-name>', '' ).replace('</last-name>', '')  			
+
+				# Определение кодировки файла
+				charset_detector = UniversalDetector()
+				with open(dirpath+i, 'rb') as fh:
+				    for line in fh:
+				        charset_detector.feed(line)
+				        if charset_detector.done:
+				            break
+				    charset_detector.close()
+				
+				file = open(dirpath+i, 'rt', encoding=charset_detector.result['encoding'])
+				cont = file.read(1000)
+				bookTitle =  re.findall('<book-title>[-—а-яА-Я0-9a-zA-Z.,?!:;_|/*<>\\\' ]+</book-title>', cont)[0].replace('<book-title>', '' ).replace('</book-title>', '')
+				firstName = re.findall('<first-name>[-—а-яА-Я0-9a-zA-Z.,?!:;_|/*<>\\\' ]+</first-name>', cont)[0].replace('<first-name>', '' ).replace('</first-name>', '')
+				lastName = re.findall('<last-name>[-—а-яА-Я0-9a-zA-Z.,?!:;_|/*<>\\\' ]+</last-name>', cont)[0].replace('<last-name>', '' ).replace('</last-name>', '')  
+
 				newName = bookTitle +  ". " + firstName + " "+ lastName + '.fb2'
 				newName = re.sub('[<?>:"\\|/*]', '', newName)
 				file.close()
-				os.rename(dirpath+i, dirpath + newName )
-				print(i+Fore.GREEN+' --> '+Fore.WHITE+newName)
+
+				if i!=newName: 
+					os.rename(dirpath+i, dirpath + newName )					
+					print(i+Fore.GREEN+' --> '+Fore.WHITE+newName)
 			except UnicodeDecodeError:
 				print(i+Fore.RED+' --> '+ '[ERROR] Файл не переименован. Неорректное содержимое')
 			except IndexError:
-				print(i+' --> '+ '[ERROR] Файл не переименован. Отсутствуют необходимые данные')
+				print(i+Fore.RED+' --> '+ '[ERROR] Файл не переименован. Отсутствуют необходимые данные')
+
 
 		if re.findall('.epub$', i):
 			try:	
 				metadata = epub_meta.get_epub_metadata(dirpath+i)
 				newName = metadata.title + '. '+', '.join(metadata.authors)+'.epub'
-				os.rename(dirpath+i, dirpath+newName)
-				print(i+Fore.GREEN+' --> '+Fore.WHITE+newName)
+
+				if i != newName:
+					os.rename(dirpath+i, dirpath+newName)
+					print(i+Fore.GREEN+' --> '+Fore.WHITE+newName)
 			except KeyError:
 				print(i+Fore.RED+' --> '+ '[ERROR] Файл не переименован. Неорректное содержимое')
-
+bar.finish()
 			
 
 	
-bar.finish()
-		
-		
-
-		
-
